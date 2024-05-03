6fef163b0818524879df8b528245b4e52c9c5363
oualid <azmioualid@gmail.com>
2024-05-03 08:20:05 -0500

    first page creation

---
+++ training/app/awesome/page.js
@@ -0,0 +1,11 @@
+// Routing works in next js by creating a new folder with page name you want. 
+// Then create a file called page.js, it behaves exactly like react.
+
+export default function awesomePage()
+{
+    return(
+        <main>
+            <h1>NextJs is Awesome</h1>
+        </main>
+    )
+}
