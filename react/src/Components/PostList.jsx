07ef5cdf0373a482ad71beeaaa63059efd4860ad
oualid <azmioualid@gmail.com>
2024-05-06 11:26:35 -0500

    lifting state finalized

--- react/src/Components/PostList.jsx
+++ react/src/Components/PostList.jsx
@@ -2,25 +2,25 @@ import React from "react";
 import Post from './Post'
 import classes from "./PostList.module.css"
 import NewPost from "./NewPost";
+import { useState } from "react";
 
 export default function UnorderedList() {
   const [enteredBody, setEnteredBody] = useState("");
-
+  const [enteredAuthor, setEnteredAuthor] = useState("");
   
   function bodyChangeHandler(e){
-    setEnteredBody(e.target.value)
+    setEnteredBody(e.target.value);
   }
   
   function authorChangeHandler(e){
-    setEnteredBody(e.target.value)
+    setEnteredAuthor(e.target.value);
   }
 
   return (
     <>
     <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler}/>
     <ul className={classes.posts}>
-      <Post author="Walid" text="Hi, I'm walid" />
-      <Post author="Tyson" text="Hi, I'm Tyson" />
+      <Post author={enteredAuthor} text={enteredBody} />
     </ul>
    </>
   );
