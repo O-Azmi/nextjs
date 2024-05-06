0e386c13c0c7a26cde879293ad204d2abbd5c190
oualid <azmioualid@gmail.com>
2024-05-06 08:15:31 -0500

    changebodyhandler function and state

--- react/src/Components/PostList.jsx
+++ react/src/Components/PostList.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Post from './Post'
 import classes from "./PostList.module.css"
+import NewPost from "./NewPost";
 
 export default function UnorderedList() {
   return (
+    <>
+    <NewPost/>
     <ul className={classes.posts}>
       <Post author="Walid" text="Hi, I'm walid" />
       <Post author="Tyson" text="Hi, I'm Tyson" />
     </ul>
+   </>
   );
 }
