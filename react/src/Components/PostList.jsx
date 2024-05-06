03ab29307c115c80a49bc0600d0e553c590ff367
oualid <azmioualid@gmail.com>
2024-05-06 13:11:15 -0500

    modal hide functionality using state added

--- react/src/Components/PostList.jsx
+++ react/src/Components/PostList.jsx
@@ -3,11 +3,16 @@ import Post from './Post'
 import classes from "./PostList.module.css"
 import NewPost from "./NewPost";
 import { useState } from "react";
+import Modal from './Modal'
 
 export default function UnorderedList() {
   const [enteredBody, setEnteredBody] = useState("");
   const [enteredAuthor, setEnteredAuthor] = useState("");
-  
+  const [modalIsVisible, setModalIsVisible] = useState(true);
+
+  function hideModalHandler(){
+    setModalIsVisible(false);
+  }
   function bodyChangeHandler(e){
     setEnteredBody(e.target.value);
   }
@@ -16,9 +21,19 @@ export default function UnorderedList() {
     setEnteredAuthor(e.target.value);
   }
 
+  let modalContent;
+
+  if (modalIsVisible){
+    modalContent = (
+      <Modal onClose={hideModalHandler}>
+      <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler}/>
+      </Modal>
+    )
+  }
+
   return (
     <>
-    <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler}/>
+    {modalContent}
     <ul className={classes.posts}>
       <Post author={enteredAuthor} text={enteredBody} />
     </ul>
