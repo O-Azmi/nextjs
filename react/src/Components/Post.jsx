b7bd951b17d94f6d77de30d4674ce85a92faf7b1
oualid <azmioualid@gmail.com>
2024-05-07 14:23:56 -0500

    backend added and fetched data

--- react/src/Components/Post.jsx
+++ react/src/Components/Post.jsx
@@ -6,7 +6,7 @@ export default function Post(props) {
   return (
     <li className={classes.post}>
       <p className={classes.author}>{props.author}</p>
-      <p className={classes.text}>{props.text}</p>
+      <p className={classes.body}>{props.body}</p>
     </li>
   );
 }
